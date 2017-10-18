1852607a2156f8edddcc2b5b1990b4df24363918
Vitali <vitali.pe@gmail.com>
2017-10-18 13:07:34 +0300

    a preview only player widget

--- src/monkeycoding/editor/player.cljs
+++ src/monkeycoding/editor/player.cljs
@@ -5,7 +5,17 @@
       [monkeycoding.player :as player]))
 
 
-(defn init-player! [dom config paused playback on-progress]
+(def config
+        (js-obj
+          "showLineNumbers" (:show-line-numbers default-config)
+          "theme" (:theme default-config)
+          "language" (:language default-config)
+          "rawConfig" (js-obj
+                            "scrollbarStyle" "overlay"
+                            "coverGutterNextToScrollbar" true)))
+
+
+(defn init-player! [dom paused playback on-progress]
   (let [player (new player/Player dom (clj->js config))]
     (when playback (.play player (clj->js playback)))
     (when paused   (.pause player))
@@ -14,20 +24,28 @@
     player))
 
 
-;; Player is just a nice React wrapper of the JS player
-(defn player [{:keys [paused playback on-progress]}]
-  (let [
-        pl (atom nil)
-        config (js-obj
-                  "showLineNumbers" (:show-line-numbers default-config)
-                  "theme" (:theme default-config)
-                  "language" (:language default-config)
-                  "rawConfig" (js-obj
-                                    "scrollbarStyle" "overlay"
-                                    "coverGutterNextToScrollbar" true))]
 
+;; player is just a nice React wrapper of the JS player
+(defn player [{:keys [paused playback on-progress]}]
+  (let [pl (atom nil)]
     (as-component {
-                    :on-mount (fn [this] (reset! pl (init-player! (r/dom-node this) config paused playback on-progress)))
-                    :on-props (fn [{paused :paused}] (if paused (.pause @pl) (.resume @pl)))
+                    :on-mount (fn [this] (reset! pl (init-player! (r/dom-node this) paused playback on-progress)))
+                    :on-props (fn [{paused :paused}]
+                                  (if paused
+                                    (.pause @pl)
+                                    (.resume @pl)))
+
                     :on-unmount (fn [_] (.pause @pl))
+                    :render (fn [] [:div.player-content {:style {:height "100%"}}])})))
+
+
+(defn preview-player [{:keys [playback]}]
+  (let [pl (atom nil)]
+    (as-component {
+                    :on-mount (fn [this] (reset! pl (init-player! (r/dom-node this) true playback #())))
+                    :on-props (fn [{playback :playback}]
+                                (doto @pl
+                                  (.play playback (clj->js playback))
+                                  (.pause)))
+
                     :render (fn [] [:div.player-content {:style {:height "100%"}}])})))
