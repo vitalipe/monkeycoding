7e36bdb35f108dd3fbb4f37fae37cd998aebe1bc
Vitali <vitali.pe@gmail.com>
2017-10-20 17:37:59 +0300

    upgrade reagent

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
 
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.908"]
-                 [reagent "0.7.0"]
+                 [reagent "0.8.0-alpha2"]
                  [cljsjs/codemirror "5.24.0-1"]]
 
   :plugins [[lein-cljsbuild "1.1.5"]
