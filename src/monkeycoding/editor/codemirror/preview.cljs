d102eacfa9427cacac098fd0dec6536ed9865a79
vitali <vitali.pe@gmail.com>
2017-11-13 18:09:46 +0200

    add generic code preview components

---
+++ src/monkeycoding/editor/codemirror/preview.cljs
@@ -0,0 +1,37 @@
+(ns monkeycoding.editor.codemirror.preview
+  (:require
+    [reagent.core :as r]
+    [monkeycoding.util              :refer [as-component]]
+    [monkeycoding.editor.codemirror :refer [create-codemirror!]]))
+
+
+
+(defn code-preview [{:keys [code language]}]
+  (let [
+        cm (r/atom nil)
+        config {
+                :theme "seti"
+                :read-only true
+                :show-line-numbers false
+                :language language}]
+
+    (as-component {
+                    :on-mount (fn [this]
+                                (reset! cm (create-codemirror! (r/dom-node this) config))
+                                (.setValue @cm code))
+
+                    :on-props  (fn [{:keys [code language]}]
+                                  (doto @cm
+                                    (.setValue code)))
+
+                    :render   (fn []
+                                [:div.code-preview])})))
+
+
+
+(defn html-preview [html]
+  [code-preview {:code html :language "htmlmixed"}])
+
+
+(defn js-preview [code]
+  [code-preview {:code code :language "javascript"}])
