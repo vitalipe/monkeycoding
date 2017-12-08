11d27b063f0a1a10a2f6be1f3956de18fd5ec81f
vitali <vitali.pe@gmail.com>
2017-12-08 11:48:44 +0200

    add label & boolean option widgets

--- src/monkeycoding/widgets/option.cljs
+++ src/monkeycoding/widgets/option.cljs
@@ -1,9 +1,31 @@
 (ns monkeycoding.widgets.option
-    (:require))
-
+  (:require
+    [monkeycoding.widgets.label :refer [editable-label]]))
 
 (defn option-item [title widget]
   [:div.option-item
     [:label.title title]
     [:div.padding]
     widget])
+
+
+
+(defn label-option [{:keys [title value on-change on-edit]
+                     :or {on-edit identity on-change identity}}]
+  [option-item title
+    [editable-label {:value value :on-edit on-edit :on-change on-change}]])
+
+
+(defn boolean-option [{:keys [title value on-change]}]
+  [option-item
+    title
+    [:div.boolean-switch
+      [:label.bool-false {
+                          :on-click #(on-change false)
+                          :class (when-not value "active")}
+        "false"]
+      [:label.spacer "|"]
+      [:label.bool-true {
+                          :on-click #(on-change true)
+                          :class (when value "active")}
+        "true"]]])
