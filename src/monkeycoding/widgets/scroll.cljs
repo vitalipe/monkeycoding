0c7a51dfa287306bb2f6a3f7835aa46f3e90c7ab
vitalipe <vitali.pe@gmail.com>
2017-12-17 17:46:25 +0200

    add drag-scroll-panel widget

--- src/monkeycoding/widgets/scroll.cljs
+++ src/monkeycoding/widgets/scroll.cljs
@@ -5,9 +5,6 @@
 
 
 
-
-;; widgets
-
 (defn scroll-panel [_]
   (as-component {
                   :on-mount #(new js/SimpleBar (r/dom-node %))
@@ -15,3 +12,22 @@
                             (if (map? props)
                               (into [] (concat [:div.scroll-panel props] children))
                               (into [] (concat [:div.scroll-panel] [props] children))))}))
+
+
+(defn drag-scroll-panel [{ :keys [scrollable]}]
+  (let [
+        last-width (r/atom nil)
+        sync! #(when (= @last-width (.-scrollWidth %)
+                       (.reset js/dragscroll)
+                       (reset! last-width (.-scrollWidth %))))]
+    (as-component {
+                    :on-mount #(.reset js/dragscroll)
+                    :on-props #(do
+                                 (when-not (:scrollable %1) (set! (.-scrollLeft (r/dom-node %2)) 0))
+                                 (sync! (r/dom-node %2)))
+
+                    :after-render #(sync! (r/dom-node %))
+                    :render (fn [{ :keys [scrollable] :as props} & children]
+                              (if-not scrollable
+                                (apply vector :div (dissoc props :scrollable) children)
+                                (apply vector :div.dragscroll (dissoc props :scrollable) children)))})))
--- src/monkeycoding/widgets/util.cljs
+++ src/monkeycoding/widgets/util.cljs
@@ -7,11 +7,13 @@
 
 
 (defn as-component [{:keys [on-mount
+                            after-render
                             on-props
                             on-unmount
-                            render] :or {on-unmount #() on-mount #() on-props #()}}]
+                            render] :or {after-render #() on-unmount #() on-mount #() on-props #()}}]
   (create-class {
                   :component-did-mount on-mount
-                  :component-will-receive-props #(on-props (second %2))
+                  :component-did-update after-render
+                  :component-will-receive-props #(on-props (second %2) %1)
                   :component-will-unmount on-unmount
                   :reagent-render render}))
