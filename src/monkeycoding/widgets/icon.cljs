6b4b916d3e95beda5c2c7a1acce396df78d1af8f
vitali <vitali.pe@gmail.com>
2017-12-29 10:50:19 +0200

    icon can have a custom class name

--- src/monkeycoding/widgets/icon.cljs
+++ src/monkeycoding/widgets/icon.cljs
@@ -1,34 +1,36 @@
 (ns monkeycoding.widgets.icon)
 
 
-(defn icon [icon-name]
-  (->> (case icon-name
-          :export      "android-share-alt"
-          :commit      "merge"
-          :redo        "forward"
-          :undo        "reply"
-          :record      "record"
-          :recording   "ios-albums"
-          :menu        "navicon"
-          :play        "play"
-          :pause       "ios-pause"
-          :goto-next   "play.adjust-smaller"
-          :goto-prv    "play.horizontal-flip.adjust-smaller"
-          :goto-start  "ios-skipbackward"
-          :goto-end    "ios-skipforward"
-          :timeline    "ios-pulse-strong"
-          :close       "close"
-          :add-mark    "code"
-          :marks       "code-working"
-          :delta-time  "android-stopwatch"
-          :squash      "arrow-shrink"
-          :arrow-down  "ios-arrow-down"
-          :checked     "checkmark"
-          :settings    "wrench"
-          :about       "ios-heart"
-          icon-name)
-
-    (str ".icon" ".ion-")
-    (str "i." "icon-" (name icon-name))
-    (keyword)
-    (vector)))
+(defn icon [props]
+  (if-not (map? props)
+    (icon {:icon props})
+    (->> (case (:icon props)
+            :export      "android-share-alt"
+            :commit      "merge"
+            :redo        "forward"
+            :undo        "reply"
+            :record      "record"
+            :recording   "ios-albums"
+            :menu        "navicon"
+            :play        "play"
+            :pause       "ios-pause"
+            :goto-next   "play.adjust-smaller"
+            :goto-prv    "play.horizontal-flip.adjust-smaller"
+            :goto-start  "ios-skipbackward"
+            :goto-end    "ios-skipforward"
+            :timeline    "ios-pulse-strong"
+            :close       "close"
+            :add-mark    "code"
+            :marks       "code-working"
+            :delta-time  "android-stopwatch"
+            :squash      "arrow-shrink"
+            :arrow-down  "ios-arrow-down"
+            :checked     "checkmark"
+            :settings    "wrench"
+            :about       "ios-heart"
+            (:icon props))
+      (str ".icon" ".ion-")
+      (str "." (:class props))
+      (str "i." "icon-" (name (:icon props)))
+      (keyword)
+      (vector))))
