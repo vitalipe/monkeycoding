d7f9b2f92d4d31039e41994f62a83d6d252f99e5
vitalipe <vitali.pe@gmail.com>
2018-01-09 12:56:41 +0200

    add playback offset

--- src/monkeycoding/editor/stream.cljs
+++ src/monkeycoding/editor/stream.cljs
@@ -88,11 +88,29 @@
 
 
 ;; stream
-(defn stream->playback [{:keys [marks-data inputs initial]}]
-  (clj->js {
-             "initial"   (update initial :marks (partial map encoded-snapshot))
-             "inputs"    (inputs->compact-inputs inputs)
-             "marksInfo" (reduce-kv #(assoc %1 %2 (dissoc %3 :inserted-at)) {} marks-data)}))
+(defn squash [{:keys [inputs marks-data next-mark-data-id]} index]
+  (let [
+        [_ rest-inputs] (split-at (inc index) inputs)
+        initial (get-in inputs [index :snapshot])
+        marks-data (reduce-kv
+                     (fn [m id mark]
+                       (assoc m id
+                         (update mark :inserted-at #(max -1 (- % 1 index)))))
+                     {} marks-data)]
+
+    {:initial initial
+     :inputs (into [] rest-inputs)
+     :marks-data marks-data
+     :next-mark-data-id next-mark-data-id}))
+
+
+(defn stream->playback
+  ([stream index] (stream->playback (squash stream index)))
+  ([{:keys [marks-data inputs initial]}]
+   (clj->js {
+              "initial"   (update initial :marks (partial map encoded-snapshot))
+              "inputs"    (inputs->compact-inputs inputs)
+              "marksInfo" (reduce-kv #(assoc %1 %2 (dissoc %3 :inserted-at)) {} marks-data)})))
 
 
 (defn stream->playback-snapshot [{:keys [initial inputs marks-data]} index]
@@ -138,19 +156,3 @@
 
 (defn append-input [stream step snapshot dt]
   (update stream :inputs conj (merge step {:snapshot snapshot, :dt dt})))
-
-
-(defn squash [{:keys [inputs marks-data next-mark-data-id]} index]
-  (let [
-        [_ rest-inputs] (split-at (inc index) inputs)
-        initial (get-in inputs [index :snapshot])
-        marks-data (reduce-kv
-                     (fn [m id mark]
-                       (assoc m id
-                         (update mark :inserted-at #(max -1 (- % 1 index)))))
-                     {} marks-data)]
-
-    {:initial initial
-     :inputs (into [] rest-inputs)
-     :marks-data marks-data
-     :next-mark-data-id next-mark-data-id}))
